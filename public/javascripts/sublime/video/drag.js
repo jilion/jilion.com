132bf99027ff56551e3d43a7eff14d9f7fc4f96a
Zeno Crivelli <zeno@zeno.name>
2010-01-29 12:20:49 +0100

    Firefox drag controls support

--- public/javascripts/sublime/video/drag.js
+++ public/javascripts/sublime/video/drag.js
@@ -1,10 +1,14 @@
-S2.UI.Behavior.WebkitTransformDrag = Class.create(S2.UI.Behavior, {
+S2.UI.Behavior.TransformDrag = Class.create(S2.UI.Behavior, {
   initialize: function($super, element, options) {
     this.__onmousemove = this._onmousemove.bind(this);
     $super(element, options);
     this.element.addClassName('ui-draggable');
+    
     // Jilion
-    this.element.setStyle({WebkitTransform:'translate(0px,0px)'});
+    this.element.setStyle({
+      WebkitTransform:'translate(0px,0px)',
+      MozTransform:'translate(0px,0px)'
+    });
   },
 
   destroy: function($super) {
@@ -13,20 +17,20 @@ S2.UI.Behavior.WebkitTransformDrag = Class.create(S2.UI.Behavior, {
   },
 
   "handle/onmousedown": function(event) {
+    var element = this.element;
     // Jilion
-    if (event.element().hasClassName('ui-draggable')) {
-      var element = this.element;
-      // Jilion
+    if (element.hasClassName('ui-draggable')) {
       var matrixRegexp = /.*translate\(\s*(-*[0-9]*)px\s*,*\s*(-*[0-9]*)px\s*\)/;
+      var tranformMatrix = element.getStyle('WebkitTransform') || element.getStyle("MozTransform");
       this._startPointer  = event.pointer();
       this._startPosition = {
-        left: window.parseInt(element.getStyle('WebkitTransform').match(matrixRegexp)[1], 10),
-        top:  window.parseInt(element.getStyle('WebkitTransform').match(matrixRegexp)[2],  10)
+        left: window.parseInt(tranformMatrix.match(matrixRegexp)[1], 10),
+        top:  window.parseInt(tranformMatrix.match(matrixRegexp)[2],  10)
         // left: window.parseInt(element.getStyle('left'), 10),
         // top:  window.parseInt(element.getStyle('top'),  10)
       };
-    
-      document.observe('mousemove', this.__onmousemove);      
+      ddd(this._startPosition)
+      document.observe('mousemove', this.__onmousemove);
     }
   },
 
@@ -45,12 +49,19 @@ S2.UI.Behavior.WebkitTransformDrag = Class.create(S2.UI.Behavior, {
       x: pointer.x - this._startPointer.x,
       y: pointer.y - this._startPointer.y
     };
-
-    var newPosition = {
-      WebkitTransform: 'translate('+ (this._startPosition.left + delta.x) + 'px, ' + (this._startPosition.top  + delta.y) + 'px)'
-      // left: (this._startPosition.left + delta.x) + 'px',
-      // top:  (this._startPosition.top  + delta.y) + 'px'
+    
+    // var newPosition = {
+    //   left: (this._startPosition.left + delta.x) + 'px',
+    //   top:  (this._startPosition.top  + delta.y) + 'px'
+    // };
+    // this.element.setStyle(newPosition);
+    
+    // Jilion
+    var translateValue = 'translate('+ (this._startPosition.left + delta.x) + 'px, ' + (this._startPosition.top  + delta.y) + 'px)';
+    var newTransformedPosition = {
+      WebkitTransform: translateValue,
+      MozTransform: translateValue
     };
-    this.element.setStyle(newPosition);
+    this.element.setStyle(newTransformedPosition);
   }
 });
