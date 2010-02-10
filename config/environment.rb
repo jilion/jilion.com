f1df9b88c0832e3d58111e7e6c6beb9791b20377
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-02-10 16:56:45 +0100

    Added notification email on contact creation Added issue number on contact model

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,8 @@ Rails::Initializer.run do |config|
   
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  # Add additional load paths for your own custom dirs
+  %w( mailers ).each { |dir| config.load_paths << "#{Rails.root}/app/#{dir}" }
   
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'haml'
