b9515909d343b7a22c59a3728c368728083e6ff7
Octave Zangs <octavez@octavez.com>
2010-02-10 15:20:01 +0100

    Views for contact

--- app/models/contact/deal.rb
+++ app/models/contact/deal.rb
@@ -1,5 +1,15 @@
 class Contact::Deal < Contact
   
+  key :name, String
+  key :organization, String
+  key :url, String
+  key :activity, String
+  key :project_description, String
   key :budget, String, :required => true
+  key :goal, String
+  key :metrics, String
+  key :challenge, String
+  key :deadline, String
+  key :comment, String
   
 end
