c02555bf31bc9a349fd7c82ea80b3cf132b25659
Rémy Coutable <remy@jilion.com>
2011-03-24 10:37:32 +0100

    Set default host for mailer
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ Jilion::Application.configure do
   
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method       = :test
+  config.action_mailer.default_url_options   = { :host => 'jilion.local' }
   
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Jilion::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'jilion.com' }
 
   # Enable threaded mode
   # config.threadsafe!
