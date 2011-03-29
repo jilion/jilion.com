363986f10044ee0277d01eb749668518f64e698a
Octave Zangs <octavez@octavez.com>
2011-03-29 15:41:07 +0200

    Added the press release

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Jilion::Application.routes.draw do
     match 'n/:n', :n => /thankyou|confirmed|unsubscribed/, :page => 'home'
     match 'ie', :page => 'ie'
     match 'team', :page => 'team'
+    match 'pr/2011-03-29', :page => 'press_2011_03_29'
     root :page => 'home'
   end
-
+  match ':p', :p => /pr|press/, :to => redirect('/pr/2011-03-29') #temporary
+  
   match 'sublime/video/flash', :to => redirect('http://sublimevideo.net/demo')
   match 'sublime(/:id)',       :to => redirect('http://sublimevideo.net')
 
